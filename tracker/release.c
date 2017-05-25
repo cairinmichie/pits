79f3eae1d76a4318ef75f4387115198e094adfe3
Cairin Michie <cairinmichie@gmail.com>
2017-05-25 17:42:08 +0200

    Test camera command.

--- tracker/release.c
+++ tracker/release.c
@@ -30,32 +30,51 @@
 #include "gps.h"
 #include "misc.h"
 
-#define RELEASE_GPIO 25 // Choose an appropriate GPIO pin - Needs to be off by default so that the burn wire doesn't activate prematurely.
+#define RELEASE_GPIO 24 // Choose an appropriate GPIO pin - Needs to be off by default so that the burn wire doesn't activate prematurely.
 #define BURN_LIMIT 10
+#define BURST_ALT 30000
+#define TEST 25
 
 void *ReleaseLoop(void *some_void_ptr)
 {
 	struct TGPS *GPS;
-    int prevAlt = 0;
 	GPS = (struct TGPS *)some_void_ptr;
+	bool burnt = false;
 
     // Code for sending a command to camera.
-    // char command[50];
-    // strcpy( command, "dir" );
-    // system(command);
-	int startTime = (unsigned)time(NULL);
+    char command[50];
+    strcpy( command, "raspivid -t 30000 -w 1280 -h 720 -fps 60 -o pivideo.h264 &" ); // & necessary otherwise system will wait for command to complete.
+
 	// This sets the GPIO pin to output mode to enable the TIP122 in the burn wire circuitry.
 	pinMode (RELEASE_GPIO, OUTPUT);
-
 	while (1) {
         // Will turn on the burn wire when the balloon reaches an altitude of 30km or if the balloon starts descending prematurely.
-        // Need to change to conditionals to include camera functionality.
-		// digitalWrite(RELEASE_GPIO, (GPS->Altitude >= 30000) || ((GPS->Altitude < prevAlt) && (GPS->Altitude > 2000)));
-        // sleep(BURN_LIMIT);
-        // digitalWrite(RELEASE_GPIO, 0);
-        // prevAlt = GPS->Altitude; // Set previous altitude to check that the ballon is still ascending.
-		if(((unsigned)time(NULL) - startTime) > 10){
-			digitalWrite(RELEASE_GPIO,1);
+		// Reached release altitude?
+		if (GPS->Altitude > BURST_ALT) {
+			if(!burnt){
+				system(command);
+				digitalWrite(RELEASE_GPIO, 1);
+				sleep(BURN_LIMIT);
+				digitalWrite(RELEASE_GPIO, 0);
+				burnt = true;
+			}
+		}
+		// Burst?
+		else if (GPS->FlightMode >= fmBurst) {
+			if(!burnt){
+				system(command);
+				digitalWrite(RELEASE_GPIO, 1);
+				sleep(BURN_LIMIT);
+				digitalWrite(RELEASE_GPIO, 0);
+				burnt = true;
+			}
+		}
+		// Test camera command.
+		if(!burnt){
+			digitalWrite(TEST, 1);
+			system(command);
+			digitalWrite(TEST, 0);
+			burnt = true;
 		}
 		sleep(1);
 	}
